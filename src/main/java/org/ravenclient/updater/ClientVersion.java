bec16c77118a62e2e4c23c8a2efd56afbae65558
Sentinel Build <sentinel@build.local>
2026-08-10 22:40:46 +0100

    Release 1.0.48

--- src/main/java/org/ravenclient/updater/ClientVersion.java
+++ src/main/java/org/ravenclient/updater/ClientVersion.java
@@ -8,7 +8,7 @@ public final class ClientVersion {
      * {@code target/RavenClient-update-<version>.zip} + a matching update.json
      * to your server so clients can self-update.
      */
-    public static final String VERSION = "1.0.47";
+    public static final String VERSION = "1.0.48";
 
     private ClientVersion() {
     }
